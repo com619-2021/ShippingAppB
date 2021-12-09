7091bb8e8488f311324929129db632be6dfe485c
Jacob.Dale <jacob.dale@qinetiq.com>
2021-12-09 09:08:09 +0000

    added a way to calculate ship details. They are arbitary numbers at the moment

--- Main/SpringExample/demo-spring-app-master/src/main/java/com/example/demo/controller/OrderShipment.java
+++ Main/SpringExample/demo-spring-app-master/src/main/java/com/example/demo/controller/OrderShipment.java
@@ -25,16 +25,58 @@ public class OrderShipment
     private Ship OrderTeuCargoShip()
     {
         var uuid = UUID.randomUUID();
-        double shipLength = 500;
-        double shipWidth = 350;
-        double shipDraft = 150;
+        ShipMeasurements shipMeasurements;
         if(this.orderShipmentDto.getNumberOfContainers() <= 15000)
         {
-            shipLength = 790;
-            shipWidth = 521;
-            shipDraft = 263.33;
+            shipMeasurements = new ShipMeasurements(263.33, 521, 790);
+        }
+        else if (this.orderShipmentDto.getNumberOfContainers() <= 20000)
+        {
+            shipMeasurements = new ShipMeasurements(467.58, 1500, 1000);
+        }
+        else
+        {
+            shipMeasurements = new ShipMeasurements(753.33, 1790, 1521);
         }
 
-        return new Ship(shipDraft, shipLength, shipWidth, uuid, this.orderShipmentDto.getShipType());
+        return new Ship(shipMeasurements.shipDraft, shipMeasurements.shipLength, shipMeasurements.shipWidth, uuid,
+                this.orderShipmentDto.getShipType());
+    }
+
+    private Ship OrderFerry()
+    {
+        var uuid = UUID.randomUUID();
+        ShipMeasurements shipMeasurements;
+        if(this.orderShipmentDto.getNumberOfPassengers() <= 1500)
+        {
+            shipMeasurements = new ShipMeasurements(263.33, 521, 790);
+        }
+        else if (this.orderShipmentDto.getNumberOfPassengers() <= 2000)
+        {
+            shipMeasurements = new ShipMeasurements(467.58, 1500, 1000);
+        }
+        else
+        {
+            shipMeasurements = new ShipMeasurements(753.33, 1790, 1521);
+        }
+
+        return new Ship(shipMeasurements.shipDraft, shipMeasurements.shipLength, shipMeasurements.shipWidth, uuid,
+                this.orderShipmentDto.getShipType());
+    }
+
+    private class ShipMeasurements
+    {
+        public double shipDraft;
+
+        public double shipLength;
+
+        public double shipWidth;
+
+        public ShipMeasurements(double draft, double length, double width)
+        {
+            this.shipDraft = draft;
+            this.shipLength = length;
+            this.shipWidth = width;
+        }
     }
 }
